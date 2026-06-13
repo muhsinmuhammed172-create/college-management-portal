05619e4109a61cf0fe9107e16c2a3d6328515c7e
Nehal <nehalmattathil@gmail.com>
2026-06-13 14:59:31 +0530

    Add Academics page with all info on departments and courses

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom'
 
 import Home from "./pages/Home";
 import PrincipalMessage from './components/PrincipalMessage';
+import Academics from "./pages/Academics";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
 
       {/* Home Page */}
       <Route path="/" element={<Home />} />
+      <Route path="/academics" element={<Academics />} />
 
       {/* Principal Message Page */}
       <Route
