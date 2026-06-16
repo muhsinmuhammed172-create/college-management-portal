c2a86800446cbe6df618e7881bc63f2ed6ff4bc6
Amal Hasin <amalhasin2004@gmail.com>
2026-06-16 19:15:33 +0530

    nothing done

---
+++ backend/Server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+// Test Route
+app.get("/", (req, res) => {
+  res.send("Backend Running...");
+});
+
+
+const PORT = process.env.PORT || 5000;
+
+
+// Database connection
+mongoose.connect(
+ process.env.MONGO_URI,
+ {
+   serverSelectionTimeoutMS: 5000
+ }
+)
+.then(() => {
+
+  console.log("MongoDB Connected");
+
+  app.listen(PORT, () => {
+    console.log(
+      `Server running on port ${PORT}`
+    );
+  });
+
+})
+.catch((err) => {
+  console.log(err);
+});
